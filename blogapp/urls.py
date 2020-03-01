b17cce574b306705d4ba92741275ed80500a8805
KMA-USS <unrulystrongsoul@gmail.com>
2020-03-02 00:47:51 +0100

    Replaced the old apps with the new ones, which makes use of python uuid for the id fields in all custom models

---
+++ blogapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    PostListView, PostDetailView,
+    AuthorDetailView,
+)
+
+app_name = 'blogapp'
+
+# Your urls here
+urlpatterns = [
+    path('blog', PostListView.as_view(), name='post-list'),
+    path('blog/read/<slug:slug>', PostDetailView.as_view(), name='post-detail'),
+    path('author/<slug:slug>', AuthorDetailView.as_view(), name='author-detail'),
+]
